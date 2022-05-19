691bd187d8812dbe2d522c571321364ad97f32b7
luciacenetiempo <luciacenetiempo@gmail.com>
2022-05-19 16:02:25 +0200

    Let the magic work!

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const body = document.querySelector('body')
 const defaultTheme = 'light'
 
-const setTheme = (theme) => {
-  localStorage.setItem('theme', theme)
-  body.setAttribute('data-theme', theme)
-}
-
 const toggleTheme = () => {
   let activeTheme = localStorage.getItem('theme')
   activeTheme == 'light' ? setTheme('dark') : setTheme('light')
+}
+
+const setTheme = (theme) => {
+  localStorage.setItem('theme', theme)
+  body.setAttribute('data-theme', theme)
 }
 
 const checkDefaultTheme = () => {
